5c9b50c853bfcf09757123625b9d2fc6a1725f3c
Logan Schmidt <logo3801@gmail.com>
2016-07-18 11:30:57 -0500

    Fixed endpoint for colectivo

--- let_me_work.sh
+++ let_me_work.sh
@@ -6,7 +6,7 @@
 #   note: you can not and should not use google for the testurl, 
 #   they have funky internal redirections setup that make this difficulty
 name=colectivo
-ip=192.168.10.1
+endpoint=http://192.168.10.1:8000/index.php
 testurl=http://github.com
 
 echo "--------------------------------"
@@ -15,14 +15,14 @@ echo "--------------------------------"
 echo ""
 echo -n "Detecting redirection... testing "
 echo $testurl
-if curl -Is location[testurl] | grep -q "HTTP/1.1 302 Found"
+if curl -Is $testurl | grep -q "HTTP/1.1 302 Found"
 then
     echo "Redirection detected!"
     curl -Is http://github.com | grep "Location:"
     echo -n "Sending magic POST request to tell "
     echo -n $name
     echo " to let you keep working..."
-    curl --request POST http://${ip} --data "accept=I have read and agree to everything below." -vv
+    curl --request POST $(endpoint) --data "accept=I+have+read+and+agree+to+everything+below." -vv
 else
-    echo "Redirection not detected. Closing"
+    echo "Redirection not detected."
 fi
